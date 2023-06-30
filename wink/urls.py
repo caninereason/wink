ca2997b000063a2ee7438d2c8c20bce841379f21
caninereason <theciaranneeson@hotmail.com>
2023-06-30 16:27:22 +0100

    add home and templates

--- wink/urls.py
+++ wink/urls.py
@@ -18,7 +18,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('', include('home.urls')),
 ]
