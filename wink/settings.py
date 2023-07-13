a24934472a44b2d3bd17d2a170a00094aaaa1645
caninereason <theciaranneeson@hotmail.com>
2023-07-13 20:18:38 +0100

    fix hosts error

--- wink/settings.py
+++ wink/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['app-wink.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['app-wink-b5e6bffc56ae.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
