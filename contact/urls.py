874b0a7b15de11b230e1f1a45356417a5fc0a844
caninereason <theciaranneeson@hotmail.com>
2023-10-19 02:03:51 +0100

    test contact emails heroku comments

--- contact/urls.py
+++ contact/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path('', views.contact_view, name='contact'),
+     path('contact/', views.contact_view, name='contact'),
 ]
--- contact/views.py
+++ contact/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .forms import ContactForm
 from django.core.mail import send_mail
 
+from django.core.mail import send_mail
+
 def contact_view(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
@@ -10,26 +12,30 @@ def contact_view(request):
             form.save()
 
             # Extract data from the form
-            subject = form.cleaned_data['subject']
             email = form.cleaned_data['email']
+            subject = form.cleaned_data['subject']
             message = form.cleaned_data['message']
 
-            # Send email
+            # Send email to you (or your support team)
             send_mail(
-                subject=f"Contact message from {name}",
+                subject=f"Contact message from {email}: {subject}",
                 message=message,
                 from_email=email,
                 recipient_list=['naraxa4790@wisnick.com'],  # Replace with your email
             )
+
+            # Send a thank you email to the user
             send_mail(
-                            subject=f"Thank You for your query",
-                            message=f"Thank You for your query, we will endeavor to contact you regarding your questions as soon as possible",
-                            from_email=f"wink.com",
-                            recipient_list=['email'],  # Replace with your email
-                        )
+                subject="Thank You for Your Query",
+                message="Thank you for your query. We will endeavor to contact you regarding your questions as soon as possible.",
+                from_email="wink.com",
+                recipient_list=[email],
+            )
+
             return render(request, 'contact/success.html')
     else:
         form = ContactForm()
 
     context = {'form': form}
     return render(request, 'contact/contact.html', context)
+
