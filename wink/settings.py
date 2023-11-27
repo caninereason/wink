a7dfb16534a9cc3d4092e5f94b8309d7c46d7d9d
caninereason <theciaranneeson@hotmail.com>
2023-11-27 17:55:21 +0000

    set SECRET_KEY heroku

--- wink/settings.py
+++ wink/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
